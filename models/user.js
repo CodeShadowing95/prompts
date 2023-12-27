374e4ab777f4eb5617e7a6c5035b823078c6f448
VastoLorde <ksenbonsakura@gmail.com>
2023-12-27 16:43:57 +0100

    FIX: update user model

--- models/user.js
+++ models/user.js
@@ -9,7 +9,9 @@ const UserSchema = new Schema({
     username: {
         type: String,
         required: [true, 'Username is required!'],
-        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+        /* The `match` property in the `username` field of the `UserSchema` is a validation rule that
+        ensures the value of the `username` field matches a specific regular expression pattern. */
+        // match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
     },
     image: {
         type: String,

e76143e78ce20a382e3b5ebda22480cff82f6993
VastoLorde <ksenbonsakura@gmail.com>
2023-12-27 16:59:50 +0100

    FIX: update user model - comments

--- models/user.js
+++ models/user.js
@@ -10,7 +10,22 @@ const UserSchema = new Schema({
         type: String,
         required: [true, 'Username is required!'],
         /* The `match` property in the `username` field of the `UserSchema` is a validation rule that
-        ensures the value of the `username` field matches a specific regular expression pattern. */
+        ensures the value of the `username` field matches a specific regular expression pattern.
+
+        ^: Asserts the start of the string.
+
+        (?=.{8,20}$): Positive lookahead assertion. Ensures that the length of the string is between 8 and 20 characters.
+
+        (?![_.]): Negative lookahead assertion. Ensures that the username does not start with an underscore (_) or a period (.).
+
+        (?!.*[_.]{2}): Negative lookahead assertion. Ensures that there are no consecutive underscores (_) or periods (.) anywhere in the string.
+
+        [a-zA-Z0-9._]+: Character class that allows alphanumeric characters, underscores (_), and periods (.). This ensures that only valid characters are allowed.
+
+        (?<![_.]): Negative lookbehind assertion. Ensures that the username does not end with an underscore (_) or a period (.).
+
+        $: Asserts the end of the string. */
+        
         // match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
         match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
     },
